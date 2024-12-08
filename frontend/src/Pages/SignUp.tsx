24625033a54017df7dde2b3f50c7e9ca67c78183
Zeke <egonzalez@student.fullsail.edu>
2024-12-08 08:55:01 -0500

    Signup page done, working on logic

--- frontend/src/Pages/SignUp.tsx
+++ frontend/src/Pages/SignUp.tsx
@@ -2,36 +2,39 @@ import styled from "styled-components";
 import InputField from "../Components/inputField";
 import { useState } from "react";
 import Button from "../Components/Button";
+import { useNavigate } from "react-router-dom";
 export default function SignUp() {
+  const navigate = useNavigate()
   const [userName, setUserName] = useState("");
   const [userEmail, setUserEmail] = useState("");
   const [userPass, setUserPass] = useState("");
   const [confirmPass, setConfirmPass] = useState("");
-  const [error, setError] = useState("")
-  const checkPasswords = ()=>{
-    if(userPass !== confirmPass){
-
-        return false
-    }else{
-        return true;
+  const [error, setError] = useState("");
+  const checkPasswords = () => {
+    if (userPass !== confirmPass) {
+      return false;
+    } else {
+      return true;
     }
-    
-
-}
+  };
+  const handleNavigateSignin=(e: React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+        e.preventDefault()
+        navigate('/signin')
+  }
   const submitForm = async (e: React.FormEvent) => {
     e.preventDefault();
-    setError('')
-    try{
-    if(!userName || !userEmail || !userPass || !confirmPass){
-        return setError("Please fill all required fields")
+    setError("");
+    try {
+      if (!userName || !userEmail || !userPass || !confirmPass) {
+        return setError("Please fill all required fields");
+      }
+      if (!checkPasswords()) {
+        return setError("Passwords Did Not Match");
+      }
+      console.log(userName, userEmail, userPass, confirmPass);
+    } catch (err: any) {
+      setError(err.message);
     }
-    if(!checkPasswords()){
-        return setError("Passwords Did Not Match")
-    }
-    console.log(userName, userEmail, userPass, confirmPass);
-}catch(err: any){
-    setError(err.message)
-}
   };
 
   return (
@@ -42,89 +45,99 @@ export default function SignUp() {
         <InputField
           type="text"
           onChange={(e) => {
-            setError("")
-            setUserName(e.target.value)
-            }
-        }
+            setError("");
+            setUserName(e.target.value);
+          }}
           value={userName}
           placeholder="User Name"
         />
         <InputField
           type="email"
           onChange={(e) => {
-            setError("")
-            setUserEmail(e.target.value)
-            }
-        }
+            setError("");
+            setUserEmail(e.target.value);
+          }}
           value={userEmail}
           placeholder="Email"
         />
         <InputField
           type="password"
           onChange={(e) => {
-            setError("")
-            setUserPass(e.target.value)
-            }
-        }
+            setError("");
+            setUserPass(e.target.value);
+          }}
           value={userPass}
           placeholder="Password"
         />
         <InputField
           type="password"
           onChange={(e) => {
-            setError("")
-            setConfirmPass(e.target.value)
-            }
-        }
+            setError("");
+            setConfirmPass(e.target.value);
+          }}
           value={confirmPass}
           placeholder="Confirm Password"
         />
-        <Button type="submit" text="Sign Up"></Button>
-        <Disclaimer>By signing up, you agree to our Terms of Service and Privacy Policy. You must be at least 18 years old to create an account.</Disclaimer>
+        <ButtonContainer>
+          <Button type="submit" text="Sign Up"></Button>
+          <Button onClick={handleNavigateSignin} type="submit" text="Sign In"></Button>
+        </ButtonContainer>
+        <Disclaimer>
+          By signing up, you agree to our Terms of Service and Privacy Policy.
+          You must be at least 18 years old to create an account.
+        </Disclaimer>
       </FormContainer>
-      
     </Container>
   );
 }
 const Container = styled.div`
-    width:100%;
-    height:100vh;
-    display:flex;
-    align-items: center;
-    justify-content: center;
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
   gap: 1.5rem;
-  width: 30%;
-  height: fit-content;
-  padding: 4rem 6rem;
+  margin: 2rem;
+  max-width: 40rem;
+  min-width: 20rem;
+  padding: 4rem;
   background-color: #66666680;
   border-radius: 20px;
   box-shadow: 5px 5px 5px #cccccc40;
 `;
 const Error = styled.p`
-    color:red;
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
-    font-size: 2rem;
-`
+  color: red;
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  font-size: 2rem;
+`;
 const Text = styled.p`
-    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-    font-size: 2rem;
-    text-align: center;
-    color:#f1f1f1;
-    letter-spacing: .25rem;
-`
+  font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  font-size: 2rem;
+  text-align: center;
+  color: #f1f1f1;
+  letter-spacing: 0.25rem;
+`;
 
 const Disclaimer = styled.p`
-    margin-top: 2rem;
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 1rem;
-    text-align: justify;
-    color:#f1f1f190;
-    letter-spacing: .15rem;
-`
+  margin-top: 2rem;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1rem;
+  text-align: justify;
+  color: #f1f1f190;
+  letter-spacing: 0.15rem;
+`;
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap:1rem;
+`;
